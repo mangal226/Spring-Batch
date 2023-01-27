6ee022d62c93b884f9ff8183a38737d88cfe969e
Hamed LY <hamed.ly@soprasteria.com>
2023-01-27 11:05:17 +0100

    Comit tp7

--- SpgBtch_TP7_Tests/SpgBtch_TP7_Testing_enonce/src/test/java/com/soprasteria/academy/tp7/TP7JobTest.java
+++ SpgBtch_TP7_Tests/SpgBtch_TP7_Testing_enonce/src/test/java/com/soprasteria/academy/tp7/TP7JobTest.java
@@ -6,17 +6,18 @@ import java.util.Map;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.batch.core.ExitStatus;
-import org.springframework.batch.core.JobExecution;
-import org.springframework.batch.core.JobParameter;
-import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.*;
 import org.springframework.batch.test.JobLauncherTestUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.soprasteria.academy.tp7.exception.TP7ExceptionNoParams;
+
+import javax.sql.DataSource;
 
 /**
  * A FAIRE
@@ -44,9 +45,35 @@ import com.soprasteria.academy.tp7.exception.TP7ExceptionNoParams;
  * 5. verifier que les deux methodes de tests s'executent sans 
  *  5.1. utiliser l'annotation dirties context pour resoudre les eventuels problèmes de contexte.
  */
-public class TP7JobTest {
 
+@ContextConfiguration(locations = { "/com/soprasteria/academy/tp7/tp7.test.context.xml",
+        "/com/soprasteria/academy/tp7/context/tp7.job.context.xml" })
+@RunWith(SpringJUnit4ClassRunner.class)
+public class TP7JobTest {
+    @Autowired
+    private JobLauncherTestUtils jobLauncherTestUtils;
+
+    private JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    DataSource dataSource;
+
+
+
+    @Test
+    @DirtiesContext
     public void testJob_OK() throws Exception {
+
+        JobParameters jobParameters = new JobParametersBuilder().addString("date", "01/01/2020").toJobParameters();
+
+        JobExecution resultat= jobLauncherTestUtils.launchJob(jobParameters);
+
+
+        //Assertions
+        //Assert.assertEquals("CECI_EST_UN_CODE_VOLONTAIREMENT_MODIFIE", resultat.getExitStatus());
+        //Assert.assertEquals("exitCode=CECI_EST_UN_CODE_VOLONTAIREMENT_MODIFIE;exitDescription=", resultat.getExitStatus());
+        Assert.assertEquals("Le code n'est pas celui attendu",
+                new ExitStatus("CECI_EST_UN_CODE_VOLONTAIREMENT_MODIFIE"), resultat.getExitStatus());
         // ARRANGE
 
         // ACT
@@ -55,6 +82,8 @@ public class TP7JobTest {
 
     }
 
+    @Test
+    @DirtiesContext
     public void testJob_KO() throws Exception {
         // ARRANGE
 
